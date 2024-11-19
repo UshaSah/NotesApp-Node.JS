64c79269c88f33b360184ed12d54f17a45cf43a8
UshaSah <usah@ucdavis.edu>
2024-11-19 00:02:14 -0800

    Argument Parsing with Yargs

---
+++ app.js
@@ -0,0 +1,80 @@
+
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
+
+//Customize yargs version
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'String'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'String'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title ', argv.title)
+        console.log('Body ' + argv.body)
+    }
+})
+
+//create remove command
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: "describe your list",
+    handler: function () {
+        console.log('Listing out all note')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading the note')
+    }
+})
+
+
+//add, remove, read, list
+console.log(yargs.argv)
+
+
+// const msg = getNotes()
+// console.log(msg)
+
+// console.log(chalk.red.bold.inverse("Error!"))
+
+// console.log(process.argv)
+
+
+
+
+
+
+
+// const validator = require('validator')
+// console.log(validator.isEmail('gmail.com'))
+
+
+// const add = require('./utils.js')
+// const sum = add(4, -2)
+// console.log(sum)
+
